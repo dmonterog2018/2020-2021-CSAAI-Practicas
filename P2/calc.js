7562450b4edfa47dd4aefaa3500ffc02ef21c06b
dmonterog2018 <d.monterog.2018@alumnos.urjc.es>
2021-04-22 13:31:26 +0200

    Empezando js y css

---
+++ P2/calc.js
@@ -0,0 +1,14 @@
+console.log("Lanzamos la calculadora....");
+
+
+display = document.getElementById("display")
+botton1 = document.getElementById("1")
+botton2 = document.getElementById("2")
+botton3 = document.getElementById("3")
+botton4 = document.getElementById("4")
+botton5 = document.getElementById("5")
+botton6 = document.getElementById("6")
+botton7 = document.getElementById("7")
+botton8 = document.getElementById("8")
+botton9 = document.getElementById("9")
+botton0 = document.getElementById("0")
