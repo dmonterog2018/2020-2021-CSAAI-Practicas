e5dd3e80f4cdd82cb93f361b7ed6830fd476a425
dmonterog2018 <d.monterog.2018@alumnos.urjc.es>
2021-04-24 12:59:54 +0200

    COMENZANDO LAS FUNCIONES

--- P2/calc.js
+++ P2/calc.js
@@ -2,13 +2,37 @@ console.log("Lanzamos la calculadora....");
 
 
 display = document.getElementById("display")
-botton1 = document.getElementById("1")
-botton2 = document.getElementById("2")
-botton3 = document.getElementById("3")
-botton4 = document.getElementById("4")
-botton5 = document.getElementById("5")
-botton6 = document.getElementById("6")
-botton7 = document.getElementById("7")
-botton8 = document.getElementById("8")
-botton9 = document.getElementById("9")
-botton0 = document.getElementById("0")
+boton = document.getElementsByClassName("digito")
+operaciones = document.getElementsByClassName("operacion")
+reset = document.getElementById("reset")
+resultado = document.getElementById("resultado")
+
+// Vamos a leer en primer lugar los digitos 
+for (i=0; i<boton.length; i++){ // Comparamaos las longitudes para que vaya leyendo los diferentes digitos
+boton[i].onclick = (ev) => {
+display.innerHTML += ev.target.value; // Mostramos el valor en el display
+}
+}
+// Vamos a leer en segundo lugar las operaciones 
+for (i=0; i<operaciones.length; i++) {
+operaciones[i].onclick = (ev) => {
+display.innerHTML += ev.target.value;
+}    
+}
+
+ 
+
+
+
+
+
+
+resultado.onclick = () => {
+console.log("Imprime resultado");
+// Falta funcion resultado
+}
+// Cuando se pulse el boton AC el valor del display volverá a 0
+reset.onclick = () => {
+    console.log("AC");
+    display.innerHTML = 0; // Imprimimos por el display el valor 0
+}
