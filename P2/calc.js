90e7f36faebec6af6653713ec3b69dbeb9bf7876
dmonterog2018 <d.monterog.2018@alumnos.urjc.es>
2021-04-28 18:12:51 +0200

    Añadiendo borrado de numeros faltan los operandos

--- P2/calc.js
+++ P2/calc.js
@@ -1,29 +1,27 @@
 console.log("Lanzamos la calculadora....");
 
-
+// Leyemos las variables del index.html
 display = document.getElementById("display")
 boton = document.getElementsByClassName("digito")
 operaciones = document.getElementsByClassName("operacion")
 reset = document.getElementById("reset")
 resultado = document.getElementById("resultado")
+borrar = document.getElementById("del")
 
 // Vamos a leer en primer lugar los digitos 
 for (i=0; i<boton.length; i++){ // Comparamaos las longitudes para que vaya leyendo los diferentes digitos
 boton[i].onclick = (ev) => {
+console.log("Digito: " + ev.target.value)
 display.innerHTML += ev.target.value; // Mostramos el valor en el display
 }
 }
 // Vamos a leer en segundo lugar las operaciones 
 for (i=0; i<operaciones.length; i++) {
 operaciones[i].onclick = (ev) => {
+console.log(" Operando: " + ev.target.value)
 display.innerHTML += ev.target.value;
 }    
 }
-
- 
-
-
-
 
 
 
@@ -36,3 +34,9 @@ reset.onclick = () => {
     console.log("AC");
     display.innerHTML = 0; // Imprimimos por el display el valor 0
 }
+// Detectamos el click del borrar
+borrar.onclick = () => {
+    console.log("Borrando");
+    display.innerHTML = display.innerHTML.slice(0, -1); // Borramos de display el ultimo dijito introducido.
+}
+
