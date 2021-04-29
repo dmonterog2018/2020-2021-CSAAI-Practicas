634baf8eecf4d7b6f86fac195d085218b4f7b20f
dmonterog2018 <d.monterog.2018@alumnos.urjc.es>
2021-04-29 18:15:50 +0200

    Raiz realizada falta css y alguna funcion mas

--- P2/calc.js
+++ P2/calc.js
@@ -7,26 +7,73 @@ operaciones = document.getElementsByClassName("operacion")
 reset = document.getElementById("reset")
 resultado = document.getElementById("resultado")
 borrar = document.getElementById("del")
+raiz = document.getElementById("raizcuadrada")
 
 // Vamos a leer en primer lugar los digitos 
 for (i=0; i<boton.length; i++){ // Comparamaos las longitudes para que vaya leyendo los diferentes digitos
 boton[i].onclick = (ev) => {
 console.log("Digito: " + ev.target.value)
-display.innerHTML += ev.target.value; // Mostramos el valor en el display
+recibido(ev.target.value); // Mostramos el valor en el display
 }
 }
 // Vamos a leer en segundo lugar las operaciones 
 for (i=0; i<operaciones.length; i++) {
 operaciones[i].onclick = (ev) => {
 console.log(" Operando: " + ev.target.value)
-display.innerHTML += ev.target.value;
+operadores(ev.target.value);
 }    
 }
+
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2_INIT: 3,
+    OP2: 4,
+}
+
+// ESTADO INICIAL DE LA CALCULADORA
+let estado = ESTADO.INIT;
+
+function recibido(boton){ //Funcion de los digitos
+    if (estado == ESTADO.INIT) { // Si es el primer digito 
+        display.innerHTML = boton; // Lo ponemos directamente en el display
+        estado = ESTADO.OP1; // Pasamos al siguiente estad
+    }else if (estado == ESTADO.OP1) { // Si el estado es OP1
+        display.innerHTML += boton;  // Sumamos el digito al display para poder generasr numero mayores a 1 decima
+    }else if (estado == ESTADO.OPERATION) { // Si detectamos el estado operacion , añadimos el digito despues del operador
+        display.innerHTML += boton;
+        estado = ESTADO.OP2_INIT;
+    }else if (estado == ESTADO.OP2_INIT) {
+        display.innerHTML += boton;
+        estado = ESTADO.OP2;
+    }else if (estado == ESTADO.OP2) {
+        display.innerHTML += boton;
+    }
+}
+
+
+function operadores(operaciones) { //Funcion de las operaciones
+    if (estado == ESTADO.OP1) {
+        display.innerHTML += operaciones;
+        estado = ESTADO.OPERATION;
+    }
+}
+
+
+
+
+
+
 
 
 
 resultado.onclick = () => {
 console.log("Imprime resultado");
+if (estado = ESTADO.OP2_INIT || estado == ESTADO.OP2) {
+    display.innerHTML = eval(display.innerHTML);
+    estado = ESTADO.OP1;
+}
 // Falta funcion resultado
 }
 // Cuando se pulse el boton AC el valor del display volverá a 0
@@ -40,3 +87,7 @@ borrar.onclick = () => {
     display.innerHTML = display.innerHTML.slice(0, -1); // Borramos de display el ultimo dijito introducido.
 }
 
+raiz.onclick = () => {
+    console.log("raiz");
+    display.innerHTML = Math.sqrt(display.innerHTML);
+}
